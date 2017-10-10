dd03594c5662feb38c24a1400655f88af26c1469
Jason <floresj4@gmail.com>
2017-10-09 23:07:45 -0400

    adding new components to modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
+import { ShoppingListComponent } from './shopping/shopping-list/shopping-list.component';
+import { ShoppingListEditorComponent } from './shopping/shopping-list-editor/shopping-list-editor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+	ShoppingListEditorComponent
   ],
   imports: [
     BrowserModule
