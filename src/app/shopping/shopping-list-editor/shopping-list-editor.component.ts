5719d4e0e152cc69d2a6184079366c7ab69629ac
Jason <floresj4@gmail.com>
2017-10-09 23:07:32 -0400

    creating shopping list editor component

---
+++ src/app/shopping/shopping-list-editor/shopping-list-editor.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-list-editor',
+  templateUrl: './shopping-list-editor.component.html',
+  styleUrls: ['./shopping-list-editor.component.css']
+})
+export class ShoppingListEditorComponent {
+
+}
