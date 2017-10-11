682a23f5aa5fe006f930dfd31795df6082979101
Jason <floresj4@gmail.com>
2017-10-10 22:12:04 -0400

    adding all components to app to ensure they are all configured and load properly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,22 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ShoppingListComponent } from './shopping/shopping-list/shopping-list.component';
 import { ShoppingListEditorComponent } from './shopping/shopping-list-editor/shopping-list-editor.component';
+import { IngredientComponent } from './shopping/ingredient/ingredient.component';
+import { RecipeComponent } from './recipe-book/recipe/recipe.component';
+import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipe-book/recipe-item/recipe-item.component';
+import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ShoppingListComponent,
-	ShoppingListEditorComponent
+	ShoppingListEditorComponent,
+	IngredientComponent,
+	RecipeComponent,
+	RecipeDetailComponent,
+	RecipeItemComponent,
+	RecipeListComponent,
   ],
   imports: [
     BrowserModule
