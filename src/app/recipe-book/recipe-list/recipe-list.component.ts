5c145d9e98d5d315639a2c4552033c0bf8eb3ba0
Jason <floresj4@gmail.com>
2017-10-14 14:05:57 -0400

    added dynamic recipe listing from static content

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -9,6 +9,11 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent {
 	recipes: Recipe[] = [
-	  new Recipe('Macaroni & cheese', 'nom nom nom', 'https://d3cizcpymoenau.cloudfront.net/images/legacy/34780/SFS_SouthernMacandCheese_20V216.jpg')
+	  new Recipe('Macaroni & cheese', 'nom nom nom', 
+	    'https://d3cizcpymoenau.cloudfront.net/images/legacy/34780/SFS_SouthernMacandCheese_20V216.jpg'),
+      new Recipe('Beef Stew', 'Thick beef stew good eaten from a bowl or poured over biscuits',
+	    'http://images.media-allrecipes.com/userphotos/720x405/729310.jpg'),
+	  new Recipe('French Onion Soup', 'Classic simple French onion soup recipe, with beef stock base, slow-cooked caramelized onions',
+	    'http://www.recipetineats.com/wp-content/uploads/2016/09/French-Onion-Soup-3-680x952.jpg')
 	];
 }
