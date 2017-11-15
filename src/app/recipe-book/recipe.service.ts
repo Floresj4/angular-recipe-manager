284c51732c8047d050adb548d0361eb06b7f1634
Jason <floresj4@gmail.com>
2017-11-15 18:32:54 -0500

    adding mock data

--- src/app/recipe-book/recipe.service.ts
+++ src/app/recipe-book/recipe.service.ts
@@ -8,17 +8,28 @@ export class RecipeService implements OnInit {
 
 	private recipes: Recipe[] = [
 	  new Recipe('Macaroni & cheese', 
-				 'nom nom nom', 
+				 'nom nom nom',
 				 'https://d3cizcpymoenau.cloudfront.net/images/legacy/34780/SFS_SouthernMacandCheese_20V216.jpg',
-				[]),
+				[
+		  new Ingredient("Pasta", 1),
+		  new Ingredient("Cheese", 2)
+	  ]),
       new Recipe('Beef Stew',
 				 'Thick beef stew good eaten from a bowl or poured over biscuits',
 				 'http://images.media-allrecipes.com/userphotos/720x405/729310.jpg',
-				[]),
+				[
+		  new Ingredient("Beef", 1),
+		  new Ingredient("Tomato", 2),
+		  new Ingredient("Pepper", 1)
+	  ]),
 	  new Recipe('French Onion Soup',
 				 'Classic simple French onion soup recipe, with beef stock base, slow-cooked caramelized onions',
 				 'http://www.recipetineats.com/wp-content/uploads/2016/09/French-Onion-Soup-3-680x952.jpg',
-				[])
+				[
+		  new Ingredient("Onions", 3),
+		  new Ingredient("Beef stock", 2),
+		  new Ingredient("Cheese", 2)
+	  ])
 	];
 	
 	constructor() {
