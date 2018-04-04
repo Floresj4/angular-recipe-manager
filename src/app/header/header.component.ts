abd4c238413ff77c87bb1d21a0efe3a4f3bc9047
Jason <floresj4@gmail.com>
2018-04-04 19:18:49 -0400

    starting to conditionally trigger service based root url; just an idea

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {RecipeService} from '../recipe-book/recipe.service';
+import {ShoppingService} from '../shopping/shopping.service';
+import {ActivatedRoute, Router, Event, NavigationEnd} from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private recipeService: RecipeService,
+              private shoppingListService: ShoppingService,
+              private router: Router,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.router.events.subscribe((event: Event) => {
+      if(event instanceof NavigationEnd) {
+        let root = this.getNavigationRoot(event.urlAfterRedirects);
+        console.log(root);
+      }
+    });
+  }
+
+  /**
+   * get the navigation root to determine which service to invoke
+   */
+  getNavigationRoot(url: string) {
+    for(let i = 1; i < url.length; i++) {
+      let char = url.charAt(i);
+      if(char === '?' || char === '/')
+        return url.substring(1, i - 1);
+    }
+
+    return url.substr(1);
   }
 
   onSave() {
