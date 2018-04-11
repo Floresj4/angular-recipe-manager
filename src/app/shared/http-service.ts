cec83ca887902e58cb74ee8038913a5450b3b81d
Jason <floresj4@gmail.com>
2018-04-11 00:05:00 -0400

    comments...

--- src/app/shared/http-service.ts
+++ src/app/shared/http-service.ts
@@ -1,3 +1,10 @@
+/**
+ * interface for services interacting with a datasource.  Because
+ * of the nature of routing, orchestrated via header.component, this
+ * definition serves to simplify the interactions with Manage dropdown
+ * functionality.  Each implementation, injectable service, should
+ * encapsulate and manage its own data lifecycle.
+ */
 export interface IHttpService {
   save();
   fetch();
