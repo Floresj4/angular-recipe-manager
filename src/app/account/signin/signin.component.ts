872557eeadd0c3cba705125fdb4b47b7dd5a4b93
Jason <floresj4@gmail.com>
2018-04-13 17:38:56 -0400

    binding form and adding required attributes

--- src/app/account/signin/signin.component.ts
+++ src/app/account/signin/signin.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninComponent implements OnInit {
 
+  @ViewChild('signinForm') signinForm: NgForm;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSignIn() {
+
+  }
 }
