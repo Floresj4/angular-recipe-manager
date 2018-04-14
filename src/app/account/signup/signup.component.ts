5b07b97201641b50d7133e960538e369b5b20dc5
Jason <floresj4@gmail.com>
2018-04-13 23:11:02 -0400

    removing unnecessary form fields and implementing firebase signup

--- src/app/account/signup/signup.component.ts
+++ src/app/account/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {AuthService} from '../auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -10,32 +11,20 @@ export class SignupComponent implements OnInit {
 
   signupForm: FormGroup;
 
-  constructor() { }
+  constructor(private auth: AuthService) { }
 
   ngOnInit() {
     this.signupForm = new FormGroup({
-      'firstname': new FormControl(null, Validators.required),
-      'lastname': new FormControl(null, Validators.required),
       'email': new FormControl(null, [Validators.required, Validators.email]),
       'password': new FormControl(null, Validators.required),
-      'cwpassword': new FormControl(null, Validators.required),
     });
   }
 
   onSubmit() {
 
-    if(this.signupForm.valid && this.passwordsAreValid()) {
-      console.log(this.signupForm.value);
+    if (this.signupForm.valid) {
+      this.auth.signUpUser(this.signupForm.value.email,
+        this.signupForm.value.password);
     }
-  }
-
-  /**
-   * simple check for password equality
-   * @returns {boolean}
-   */
-  passwordsAreValid() {
-    let pw = this.signupForm.value.password;
-    let cw = this.signupForm.value.cwpassword;
-    return pw === cw;
   }
 }
