d6ec63a58cb3c8f1fb9eef3c2ccd99125fd579ad
Jason <floresj4@gmail.com>
2018-04-13 22:53:52 -0400

    updating ignore file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RecipeService } from './recipe-book/recipe.service';
 import { ShoppingService } from './shopping/shopping.service';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -8,20 +9,24 @@ import { ShoppingService } from './shopping/shopping.service';
   styleUrls: ['./app.component.css'],
   providers: [RecipeService, ShoppingService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   //default to recipes first
   currentNavigation:string = 'Shopping List';
-  
+
+  ngOnInit() {
+    //add firebase api key here
+  }
+
   onNavigationEvent(event) {
     this.currentNavigation = event;
   }
-  
+
   getRecipeVisibility() {
     return this.currentNavigation == 'Recipes'
 	  ? { display: 'inline' } : { display: 'none' };
   }
-  
+
   getShoppingListVisibility() {
     return this.currentNavigation == 'Shopping List'
 	  ? { display: 'inline' } : { display: 'none' };
