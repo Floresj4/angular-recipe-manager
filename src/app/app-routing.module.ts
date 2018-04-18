c36237ba5d636699f60d6c22a2d3741096e624d5
Jason <floresj4@gmail.com>
2018-04-18 19:02:46 -0400

    cleaning up import and code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RecipesComponent } from './recipe-book/recipes/recipes.component';
 import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
-import { RecipeResolver } from './recipe-book/recipe-resolver.service';
 import { ShoppingListComponent } from './shopping/shopping-list/shopping-list.component';
-import {RecipeStartComponent} from './recipe-book/recipe-start/recipe-start.component';
-import {RecipeEditComponent} from './recipe-book/recipe-edit/recipe-edit.component';
-import {SignupComponent} from './account/signup/signup.component';
-import {SigninComponent} from './account/signin/signin.component';
-import {AuthGuard} from './account/auth-guard.service';
+import { RecipeStartComponent } from './recipe-book/recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipe-book/recipe-edit/recipe-edit.component';
+import { SignupComponent } from './account/signup/signup.component';
+import { SigninComponent } from './account/signin/signin.component';
+import { AuthGuard } from './account/auth-guard.service';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
