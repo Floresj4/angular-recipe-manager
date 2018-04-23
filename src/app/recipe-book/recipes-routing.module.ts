1fee9f883522169a899ac8704f1fc815c431af37
Jason <floresj4@gmail.com>
2018-04-23 19:09:42 -0400

    adding lazy loading

--- src/app/recipe-book/recipes-routing.module.ts
+++ src/app/recipe-book/recipes-routing.module.ts
@@ -7,7 +7,7 @@ import {AuthGuard} from '../account/auth-guard.service';
 import {RecipeDetailComponent} from './recipe-detail/recipe-detail.component';
 
 const recipesRoutes: Routes = [
-  { path: 'recipes', component: RecipesComponent, children: [
+  { path: '', component: RecipesComponent, children: [
       {path: '', component: RecipeStartComponent,  },
       {path: 'new', component: RecipeEditComponent, canActivate: [AuthGuard] }, //order matters in terms of parsing 'new' versus resolving an ID
       {path: ':id', component: RecipeDetailComponent },
