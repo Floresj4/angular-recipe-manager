b0215745f75006738a958daf4af0b5e20f083aae
Jason <floresj4@gmail.com>
2018-05-02 18:46:49 -0400

    moving providers into core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RecipeService } from './recipe-book/recipe.service';
-import { RecipeResolver } from './recipe-book/recipe-resolver.service';
-import { AuthService } from './account/auth.service';
-import { AuthGuard } from './account/auth-guard.service';
 import { RecipesModule } from './recipe-book/recipes.module';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping/shopping-list.module';
 import { AccountModule } from './account/account.module';
-import {CoreModule} from './core/core.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -29,7 +24,6 @@ import {CoreModule} from './core/core.module';
     RecipesModule,
     ShoppingListModule,
   ],
-  providers: [RecipeService, RecipeResolver, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,6 +3,10 @@ import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { SharedModule } from '../shared/shared.module';
 import { AppRoutingModule } from '../app-routing.module';
+import {RecipeResolver} from '../recipe-book/recipe-resolver.service';
+import {RecipeService} from '../recipe-book/recipe.service';
+import {AuthGuard} from '../account/auth-guard.service';
+import {AuthService} from '../account/auth.service';
 
 @NgModule({
   declarations: [
@@ -16,6 +20,12 @@ import { AppRoutingModule } from '../app-routing.module';
   exports: [
     AppRoutingModule,
     HeaderComponent
+  ],
+  providers: [
+    RecipeService,
+    RecipeResolver,
+    AuthService,
+    AuthGuard
   ]
 })
 export class CoreModule {
